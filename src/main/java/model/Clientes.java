2a481132c68d97dd85526baab1ee86c7551896c7
DillanSnayder <DILLANBUITRAGO@GMAIL.COM>
2024-02-11 15:45:18 -0500

    holi

--- src/main/java/model/Clientes.java
+++ src/main/java/model/Clientes.java
@@ -1,4 +1,17 @@
 package model;
 
 public class Clientes {
+    private String nombre;
+    private String apellidos;
+    private String identificacion;
+    private String direccion;
+    private String telefono;
+
+    public Clientes(String nombre, String apellidos, String identificacion, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.identificacion = identificacion;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
 }
